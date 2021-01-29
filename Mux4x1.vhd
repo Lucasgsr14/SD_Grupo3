29d65c02ff019df89ee52f942ddde715395a2622
Samuel Victor Maciel da Silva <30415578+samuelvictor001@users.noreply.github.com>
2021-01-29 04:23:04 -0300

    Add files via upload

---
+++ Mux4x1.vhd
@@ -0,0 +1,19 @@
+entity mux4x1 is
+   port(A0,A1,A2,A3 : in bit;
+	     sel : in bit_vector(1 downto 0);    
+      	Z : out bit);
+
+end mux4x1;
+
+architecture ARKMUX of mux4x1 is
+  
+     begin
+	with sel select
+  	Z<= A0 when "00"
+	    A1 when "01"
+	    A2 when "10"
+	    A3 when "others"
+    
+end ARKMUX;
+        
+
