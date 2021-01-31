45237a33b716ec4e9adbceb99de04f38cc13d9e9
Samuel Victor Maciel da Silva <30415578+samuelvictor001@users.noreply.github.com>
2021-01-31 00:52:06 -0300

    Update Mux6x1.vhd

--- Mux6x1.vhd
+++ Mux6x1.vhd
@@ -12,35 +12,35 @@ architecture arkmux of Mux6x1 is
  
   begin
    
-	 Saida(15)<= (not Sel(2) and not Sel(1) and not Sel(0) and M(15)) or
-	 (not Sel(2) and not Sel(1) and Sel(0) and CP(15)) or
+    Saida(15)<= (not Sel(2) and not Sel(1) and not Sel(0) and M(15)) or
+    (not Sel(2) and not Sel(1) and Sel(0) and CP(15)) or
     (not Sel(2) and Sel(1) and not Sel(0) and DR(15)) or
     (not Sel(2) and Sel(1) and Sel(0) and DL(15)) or
     (Sel(2) and not Sel(1) and not Sel(0) and Set(15)) or
     (Sel(2) and not Sel(1) and Sel(0) and Clear(15));
 	 
-	 Saida(14)<= (not Sel(2) and not Sel(1) and not Sel(0) and M(14)) or
-	 (not Sel(2) and not Sel(1) and Sel(0) and CP(14)) or
+    Saida(14)<= (not Sel(2) and not Sel(1) and not Sel(0) and M(14)) or
+    (not Sel(2) and not Sel(1) and Sel(0) and CP(14)) or
     (not Sel(2) and Sel(1) and not Sel(0) and DR(14)) or
     (not Sel(2) and Sel(1) and Sel(0) and DL(14)) or
     (Sel(2) and not Sel(1) and not Sel(0) and Set(14)) or
     (Sel(2) and not Sel(1) and Sel(0) and Clear(14));
 	 
-	 Saida(13)<= (not Sel(2) and not Sel(1) and not Sel(0) and M(13)) or
-	 (not Sel(2) and not Sel(1) and Sel(0) and CP(13)) or
+    Saida(13)<= (not Sel(2) and not Sel(1) and not Sel(0) and M(13)) or
+    (not Sel(2) and not Sel(1) and Sel(0) and CP(13)) or
     (not Sel(2) and Sel(1) and not Sel(0) and DR(13)) or
     (not Sel(2) and Sel(1) and Sel(0) and DL(13)) or
     (Sel(2) and not Sel(1) and not Sel(0) and Set(13)) or
     (Sel(2) and not Sel(1) and Sel(0) and Clear(13));
    
-	 Saida(12)<= (not Sel(2) and not Sel(1) and not Sel(0) and M(12)) or
-	 (not Sel(2) and not Sel(1) and Sel(0) and CP(12)) or
+    Saida(12)<= (not Sel(2) and not Sel(1) and not Sel(0) and M(12)) or
+    (not Sel(2) and not Sel(1) and Sel(0) and CP(12)) or
     (not Sel(2) and Sel(1) and not Sel(0) and DR(12)) or
     (not Sel(2) and Sel(1) and Sel(0) and DL(12)) or
     (Sel(2) and not Sel(1) and not Sel(0) and Set(12)) or
     (Sel(2) and not Sel(1) and Sel(0) and Clear(12));
     
-	 Saida(11)<= (not Sel(2) and not Sel(1) and not Sel(0) and M(11)) or
+    Saida(11)<= (not Sel(2) and not Sel(1) and not Sel(0) and M(11)) or
     (not Sel(2) and not Sel(1) and Sel(0) and CP(11)) or
     (not Sel(2) and Sel(1) and not Sel(0) and DR(11)) or
     (not Sel(2) and Sel(1) and Sel(0) and DL(11)) or
@@ -61,63 +61,63 @@ architecture arkmux of Mux6x1 is
     (Sel(2) and not Sel(1) and not Sel(0) and Set(9)) or
     (Sel(2) and not Sel(1) and Sel(0) and Clear(9));
 	 
-	 Saida(8)<= (not Sel(2) and not Sel(1) and not Sel(0) and M(8)) or
+    Saida(8)<= (not Sel(2) and not Sel(1) and not Sel(0) and M(8)) or
     (not Sel(2) and not Sel(1) and Sel(0) and CP(8)) or
     (not Sel(2) and Sel(1) and not Sel(0) and DR(8)) or
     (not Sel(2) and Sel(1) and Sel(0) and DL(8)) or
     (Sel(2) and not Sel(1) and not Sel(0) and Set(8)) or
     (Sel(2) and not Sel(1) and Sel(0) and Clear(8));
 	 
-	 Saida(7)<= (not Sel(2) and not Sel(1) and not Sel(0) and M(7)) or
+    Saida(7)<= (not Sel(2) and not Sel(1) and not Sel(0) and M(7)) or
     (not Sel(2) and not Sel(1) and Sel(0) and CP(7)) or
     (not Sel(2) and Sel(1) and not Sel(0) and DR(7)) or
     (not Sel(2) and Sel(1) and Sel(0) and DL(7)) or
     (Sel(2) and not Sel(1) and not Sel(0) and Set(7)) or
     (Sel(2) and not Sel(1) and Sel(0) and Clear(7));
 	 
-	 Saida(6)<= (not Sel(2) and not Sel(1) and not Sel(0) and M(6)) or
+    Saida(6)<= (not Sel(2) and not Sel(1) and not Sel(0) and M(6)) or
     (not Sel(2) and not Sel(1) and Sel(0) and CP(6)) or
     (not Sel(2) and Sel(1) and not Sel(0) and DR(6)) or
     (not Sel(2) and Sel(1) and Sel(0) and DL(6)) or
     (Sel(2) and not Sel(1) and not Sel(0) and Set(6)) or
     (Sel(2) and not Sel(1) and Sel(0) and Clear(6));
 	 
-	 Saida(5)<= (not Sel(2) and not Sel(1) and not Sel(0) and M(5)) or
+    Saida(5)<= (not Sel(2) and not Sel(1) and not Sel(0) and M(5)) or
     (not Sel(2) and not Sel(1) and Sel(0) and CP(5)) or
     (not Sel(2) and Sel(1) and not Sel(0) and DR(5)) or
     (not Sel(2) and Sel(1) and Sel(0) and DL(5)) or
     (Sel(2) and not Sel(1) and not Sel(0) and Set(5)) or
     (Sel(2) and not Sel(1) and Sel(0) and Clear(5));
 	 
-	 Saida(4)<= (not Sel(2) and not Sel(1) and not Sel(0) and M(4)) or
+    Saida(4)<= (not Sel(2) and not Sel(1) and not Sel(0) and M(4)) or
     (not Sel(2) and not Sel(1) and Sel(0) and CP(4)) or
     (not Sel(2) and Sel(1) and not Sel(0) and DR(4)) or
     (not Sel(2) and Sel(1) and Sel(0) and DL(4)) or
     (Sel(2) and not Sel(1) and not Sel(0) and Set(4)) or
     (Sel(2) and not Sel(1) and Sel(0) and Clear(4));
 	 
-	 Saida(3)<= (not Sel(2) and not Sel(1) and not Sel(0) and M(3)) or
+    Saida(3)<= (not Sel(2) and not Sel(1) and not Sel(0) and M(3)) or
     (not Sel(2) and not Sel(1) and Sel(0) and CP(3)) or
     (not Sel(2) and Sel(1) and not Sel(0) and DR(3)) or
     (not Sel(2) and Sel(1) and Sel(0) and DL(3)) or
     (Sel(2) and not Sel(1) and not Sel(0) and Set(3)) or
     (Sel(2) and not Sel(1) and Sel(0) and Clear(3));
 	 
-	 Saida(2)<= (not Sel(2) and not Sel(1) and not Sel(0) and M(2)) or
+    Saida(2)<= (not Sel(2) and not Sel(1) and not Sel(0) and M(2)) or
     (not Sel(2) and not Sel(1) and Sel(0) and CP(2)) or
     (not Sel(2) and Sel(1) and not Sel(0) and DR(2)) or
     (not Sel(2) and Sel(1) and Sel(0) and DL(2)) or
     (Sel(2) and not Sel(1) and not Sel(0) and Set(2)) or
     (Sel(2) and not Sel(1) and Sel(0) and Clear(2));
 	 
-	 Saida(1)<= (not Sel(2) and not Sel(1) and not Sel(0) and M(1)) or
+    Saida(1)<= (not Sel(2) and not Sel(1) and not Sel(0) and M(1)) or
     (not Sel(2) and not Sel(1) and Sel(0) and CP(1)) or
     (not Sel(2) and Sel(1) and not Sel(0) and DR(1)) or
     (not Sel(2) and Sel(1) and Sel(0) and DL(1)) or
     (Sel(2) and not Sel(1) and not Sel(0) and Set(1)) or
     (Sel(2) and not Sel(1) and Sel(0) and Clear(1));
 	 
-	 Saida(0)<= (not Sel(2) and not Sel(1) and not Sel(0) and M(0)) or
+    Saida(0)<= (not Sel(2) and not Sel(1) and not Sel(0) and M(0)) or
     (not Sel(2) and not Sel(1) and Sel(0) and CP(0)) or
     (not Sel(2) and Sel(1) and not Sel(0) and DR(0)) or
     (not Sel(2) and Sel(1) and Sel(0) and DL(0)) or
