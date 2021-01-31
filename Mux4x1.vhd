a1eb1e8445daa33146554b9444138b6bf23582f9
Samuel Victor Maciel da Silva <30415578+samuelvictor001@users.noreply.github.com>
2021-01-31 16:39:02 -0300

    Update Mux4x1.vhd

--- Mux4x1.vhd
+++ Mux4x1.vhd
@@ -1,5 +1,5 @@
-entity mux3x1 is
-   port(A0,A1,A2 : in bit_vector(16 downto 0);
+entity mux4x1 is
+   port(A0,A1,A2,A3 : in bit_vector(16 downto 0);
 	     sel : in bit_vector(1 downto 0);    
       	Z : out bit_vector(16 downto 0));
 
@@ -9,9 +9,10 @@ architecture ARKMUX of mux4x1 is
   
      begin
 	with sel select
-  	Z<= A0 when "00"
-	    A1 when "01"
-	    A2 when "10"
+  	Z<= A0 when "00",
+	    A1 when "01",
+	    A2 when "10",
+	    A3 when others;
 end ARKMUX;
         
 
